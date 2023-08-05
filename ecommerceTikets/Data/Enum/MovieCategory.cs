74465504642bea644ec060cd1f1b11798c4c89ee
trananhduc1305 <psfananhduc05@gmail.com>
2023-08-05 14:19:42 +0700

    Add project files.

---
+++ ecommerceTikets/Data/Enum/MovieCategory.cs
@@ -0,0 +1,12 @@
+﻿namespace ecommerceTikets.Data.Enum
+{
+    public enum MovieCategory
+    {
+        Action = 1,
+        Comedy,
+        Drama,
+        Documentary,
+        Cartoon,
+        Horror
+    }
+}
